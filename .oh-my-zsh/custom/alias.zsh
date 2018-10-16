bfe5b5de40900704b9d05357b046d61cd83d4b86
Gabriel Balaudo <gbalaudo@onapsis.com>
2018-10-16 14:06:04 -0300

    Added custom aliases

---
+++ .oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,7 @@
+alias calias='~/.oh-my-zsh/custom/aliaser.sh'  # Ej: calias ll "ls -l"
+alias lalias='cat ~/.oh-my-zsh/custom/alias.zsh'
+alias p='python'
+alias gs='git status'
+alias gitc='git commit'
+alias gc='git checkout'
+alias dj='p manage.py'
