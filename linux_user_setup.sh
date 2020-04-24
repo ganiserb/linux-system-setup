acc2e86e976f1de22ba48e2dcdfe7910ca4e5bea
Gabriel Balaudo <gabriel.balaudo@gmail.com>
2020-04-24 18:52:39 -0300

    Removed nvim setup.

--- linux_user_setup.sh
+++ linux_user_setup.sh
@@ -20,12 +20,6 @@ git config --global user.name "Gabriel Balaudo"
 # Git global gitignore
 git config --global core.excludesfile '~/.gitignoreglobal'
 
-# Neovim
-mkdir -p ~/dev
-mkdir -p ~/.config/nvim/
-(cd ~/dev && git clone https://github.com/fisadev/fisa-nvim-config.git && cp fisa-nvim-config/init.vim ~/.config/nvim/)
-nvim
-
 # Upgrade pip and install useful user tools:
 python3 -m pip install --upgrade pip
 python3 -m pip install --user jupyter
